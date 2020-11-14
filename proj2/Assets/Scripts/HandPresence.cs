60413fe6ac0072af221560614bea7da6119c4346
omcgrath2014 <>
2020-11-14 11:57:08 -0800

    probe specific controller

--- proj2/Assets/Scripts/HandPresence.cs
+++ proj2/Assets/Scripts/HandPresence.cs
@@ -9,11 +9,13 @@ public class HandPresence : MonoBehaviour
     void Start()
     {
         List<InputDevice> devices = new List<InputDevice>();
-        InputDevices.GetDevices(devices);
+        InputDeviceCharacteristics rightControllerCharacteristics = InputDeviceCharacteristics.Right | InputDeviceCharacteristics.Controller;
+        // InputDevices.GetDevices(devices);
+        InputDevices.GetDevicesWithCharacteristics(rightControllerCharacteristics, devices);
         foreach (var item in devices)
         {
             Debug.Log("Hello, world.");
-            Debug.Log(item.name + item.characteristics);
+            Debug.Log(item.name + item.characteristics + item.manufacturer);
         }
     }
 
