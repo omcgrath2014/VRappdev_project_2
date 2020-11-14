e1f7e0ae3c1d478bae0bbdf828e98cbe92547fa2
omcgrath2014 <>
2020-11-14 12:13:02 -0800

    get button clicks

--- proj2/Assets/Scripts/HandPresence.cs
+++ proj2/Assets/Scripts/HandPresence.cs
@@ -5,6 +5,7 @@ using UnityEngine.XR;
 
 public class HandPresence : MonoBehaviour
 {
+    private InputDevice targetDevice;
     // Start is called before the first frame update
     void Start()
     {
@@ -17,11 +18,17 @@ public class HandPresence : MonoBehaviour
             Debug.Log("Hello, world.");
             Debug.Log(item.name + item.characteristics + item.manufacturer);
         }
+        if (devices.Count > 0)
+        {
+            targetDevice = devices[0];
+        }
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        targetDevice.TryGetFeatureValue(CommonUsages.primaryButton, out bool primaryButtonValue);
+        if (primaryButtonValue)
+            Debug.Log("Pressing Primary Button");
     }
 }
