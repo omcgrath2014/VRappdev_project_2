164fc09685fd8230a939a583f592a78000568254
omcgrath2014 <>
2020-11-14 16:51:42 -0800

    simplify logic check

--- proj2/Assets/Scripts/HandPresence.cs
+++ proj2/Assets/Scripts/HandPresence.cs
@@ -28,18 +28,31 @@ public class HandPresence : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        targetDevice.TryGetFeatureValue(CommonUsages.primary2DAxisClick, out bool primary2DAxisClick);
-        if (primary2DAxisClick)
+        //targetDevice.TryGetFeatureValue(CommonUsages.primary2DAxisClick, out bool primary2DAxisClick);
+        //if (primary2DAxisClick)
+          //  Debug.Log("Got Primary 2D Avis Click");
+
+       // targetDevice.TryGetFeatureValue(CommonUsages.trigger, out float triggerValue);
+       // if (triggerValue > 0.1f)
+        //   Debug.Log("Pressing trigger" + triggerValue);
+
+      // targetDevice.TryGetFeatureValue(CommonUsages.primary2DAxis, out Vector2 primary2DAxisValue);
+       // if (primary2DAxisValue != Vector2.zero)
+          //  Debug.Log("Pressing Primary Button" + primary2DAxisValue);
+
+
+        
+        if (targetDevice.TryGetFeatureValue(CommonUsages.primary2DAxisClick, out bool primary2DAxisClick) && primary2DAxisClick)
             Debug.Log("Got Primary 2D Avis Click");
 
-        targetDevice.TryGetFeatureValue(CommonUsages.trigger, out float triggerValue);
-        if (triggerValue > 0.1f)
-           Debug.Log("Pressing trigger" + triggerValue);
+        
+        if (targetDevice.TryGetFeatureValue(CommonUsages.trigger, out float triggerValue) && triggerValue > 0.1f)
+            Debug.Log("Pressing trigger" + triggerValue);
 
-       targetDevice.TryGetFeatureValue(CommonUsages.primary2DAxis, out Vector2 primary2DAxisValue);
-        if (primary2DAxisValue != Vector2.zero)
+      
+        if (targetDevice.TryGetFeatureValue(CommonUsages.primary2DAxis, out Vector2 primary2DAxisValue) && primary2DAxisValue != Vector2.zero)
             Debug.Log("Pressing Primary Button" + primary2DAxisValue);
 
-  
+
     }
 }
