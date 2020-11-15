e4ffc9415aa6435e87a0e67f8b7268455e7dd2f4
omcgrath2014 <>
2020-11-14 16:21:15 -0800

    Monitor three characteristics

--- proj2/Assets/Scripts/HandPresence.cs
+++ proj2/Assets/Scripts/HandPresence.cs
@@ -28,13 +28,27 @@ public class HandPresence : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        targetDevice.TryGetFeatureValue(CommonUsages.primary2DAxisClick, out bool primaryButtonValue);
-        //targetDevice.TryGetFeatureValue(CommonUsages.trigger, out float primaryButtonValue);
+        targetDevice.TryGetFeatureValue(CommonUsages.primary2DAxisClick, out bool primary2DAxisClick);
+        if (primary2DAxisClick)
+            Debug.Log("Got Primary 2D Avis Click");
+
+
+        targetDevice.TryGetFeatureValue(CommonUsages.trigger, out float triggerValue);
+        if (triggerValue > 0.1f)
+            Debug.Log("Pressing Primary Button");
+
+        targetDevice.TryGetFeatureValue(CommonUsages.primary2DAxis, out Vector2 primary2DAxisValue);
+        if (primary2DAxisValue != Vector2.zero)
+            Debug.Log("Pressing Primary Button");
+
+
+
+        // Debug.Log("Pressing Trigger");
         //Debug.Log("Got Here.");
         //Debug.Log("Next the value" + primaryButtonValue);
         //if (primaryButtonValue > 0.1f)
-            if (primaryButtonValue)
-                 Debug.Log("Pressing Primary Button");
+        //if (primaryButtonValue)
+            //     Debug.Log("Pressing Primary Button");
                // Debug.Log("Pressing Trigger");
     }
 }
